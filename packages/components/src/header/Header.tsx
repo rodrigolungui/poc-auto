d4f2ffe787ea4a580903c1b4afe339950c8f7020
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2023-05-11 15:12:45 -0300

    refactor(components)!: refactoring Header to accept just h1

--- packages/components/src/header/Header.tsx
+++ packages/components/src/header/Header.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 
 interface HeaderProps {
-  level: 1 | 2;
   text: string;
 }
 
 /**
- * Header component which supports h1 and h2
+ * Header component which supports just h1
  */
-const Header = ({ level, text }: HeaderProps) => {
-  return level === 1 ? <h1>{text}</h1> : <h2>{text}</h2>;
+const Header = ({ text }: HeaderProps) => {
+  return <h1>{text}</h1>;
 };
 
 export { Header };
